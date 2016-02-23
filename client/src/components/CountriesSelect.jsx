6994eb1db56d7a37284eb195aca576c6490aa760
MichaelMacLeod <edinburghmacleod@gmail.com>
2016-02-23 11:23:01 +0000

    Sets state to match selected country index

--- client/src/components/CountriesSelect.jsx
+++ client/src/components/CountriesSelect.jsx
@@ -1,23 +1,29 @@
 var React = require('react');
 var CountriesSelect = React.createClass({
-  render: function(){
-    // var options = [];
 
-    // for(var country of this.props.countries){
-    //   options.push(
-    //       <option> {country.name} </option>
-    //     )
-    // }
+  getInitialState: function(){
+    return {selectedIndex: null};
+  },
+
+//Get the value from the event and set it as the new state: 
+  handleChange: function(e){
+    e.preventDefault();
+    console.log('e', e.target);
+    var newIndex = e.target.value;
+    this.setState({selectedIndex: newIndex});
+  },
+
+  render: function(){
 
 //BELOW, we are creating an array of equal size to the existing REST Countries API. But we're also going through the countries and creating an option element for each one, putting them in an array and putting them inside the select tag: 
 
     var options = this.props.countries.map(function(country, index){
-      return <option key={index}> { country.name } </option>
+      return <option value={index} key={index}> { country.name } </option>
     })
 
     return(
         <div>
-          <select>
+          <select value={this.state.selectedIndex} onChange={this.handleChange}>
             { options }
           </select>
         </div>
