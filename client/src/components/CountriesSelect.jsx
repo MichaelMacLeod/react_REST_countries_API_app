8a7b8168957e16770965186945f08b3940ac8871
MichaelMacLeod <edinburghmacleod@gmail.com>
2016-02-23 11:08:54 +0000

    Added countries to countries select box

--- client/src/components/CountriesSelect.jsx
+++ client/src/components/CountriesSelect.jsx
@@ -1,9 +1,24 @@
 var React = require('react');
 var CountriesSelect = React.createClass({
   render: function(){
+    // var options = [];
+
+    // for(var country of this.props.countries){
+    //   options.push(
+    //       <option> {country.name} </option>
+    //     )
+    // }
+
+//BELOW, we are creating an array of equal size to the existing REST Countries API. But we're also going through the countries and creating an option element for each one, putting them in an array and putting them inside the select tag: 
+
+    var options = this.props.countries.map(function(country, index){
+      return <option key={index}> { country.name } </option>
+    })
+
     return(
         <div>
           <select>
+            { options }
           </select>
         </div>
       )
